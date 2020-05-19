32b4b33c97bfffe82da184859791e15a14e5bebf
Taiko2306 <taiko.aronson@gmail.com>
2020-05-19 14:01:22 -0700

    activate django-heroku

--- taikohello/settings.py
+++ taikohello/settings.py
@@ -133,3 +133,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
