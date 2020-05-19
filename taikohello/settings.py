80237f05340196ebe03d963448d169db637fd68b
Taiko2306 <taiko.aronson@gmail.com>
2020-05-19 14:27:37 -0700

    activate django-heroku

--- taikohello/settings.py
+++ taikohello/settings.py
@@ -134,8 +134,8 @@ STATICFILES_DIRS = (
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# import dj_database_url
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
 # Activate Django-Heroku
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
